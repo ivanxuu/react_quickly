065882d17ec530883cc731eed74aee8d84cf3296
Ivan H <ivan@drails.com>
2018-05-21 09:30:01 +0200

    Wrap hello world in its own module
    using import export from ES06

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,16 +20,5 @@ import "phoenix_html"
 
 // import socket from "./socket"
 
-import React from "react"
-import ReactDOM from "react-dom"
+import hello from "./hello-world"
 
-class HelloWorld extends React.Component{
-  render(){
-    return(<h1>Hello World!</h1>)
-  }
-}
-
-ReactDOM.render(
-  <HelloWorld/>,
-  document.getElementById("hello-world")
-)
